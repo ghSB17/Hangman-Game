6d4f76521567ccd1cda4b5f14538c45d8854cd33
sumathi <sumathi@rocketmail.com>
2018-02-16 10:59:08 -0500

    Hangman with design added

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -1,5 +1,5 @@
 var arrCountries= 
-            ["Afghanistan","Albania","Algeria","Andorra","Angola","Antigua","Argentina","Armenia","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bhutan","Bolivia","Bosnia","Herzegovina","Botswana","Brazil","Brunei","Bulgaria","Burkina","Burundi","Cambodia","Cameroon","Canada","Cape","Verde","Central African Rep","Chad","Chile","China","Colombia","Comoros","Congo","Congo Democratic Rep","Costa Rica","Croatia","Cuba","Cyprus","Czech","Republic","Denmark","Djibouti","Dominica","Dominican","Republic","East","Timor","Ecuador","Egypt","El","Salvador","Equatorial","Guinea","Eritrea","Estonia","Ethiopia","Fiji","Finland","France","Gabon","Gambia","Georgia","Germany","Ghana","Greece","Grenada","Guatemala","Guinea","Guinea-Bissau","Guyana","Haiti","Honduras","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Ivory Coast","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kiribati","Korea","North","Korea","South","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall","Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands","New","Zealand","Nicaragua","Niger","Nigeria","Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Romania","Russian"," Federation","Rwanda","St Kitts and Nevis","St Lucia","Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South","Africa","South","Sudan","Spain","Sri","Lanka","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Togo","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Yemen","Zambia","Zimbabwe"]
+            ["Afghanistan","Albania","Algeria","Andorra","Angola","Antigua","Argentina","Armenia","Australia","Austria","Azerbaijan","Bahamas","Bahrain","Bangladesh","Barbados","Belarus","Belgium","Belize","Benin","Bhutan","Bolivia","Bosnia Herzegovina","Botswana","Brazil","Brunei","Bulgaria","Burkina","Burundi","Cambodia","Cameroon","Canada","Cape Verde","Central African Rep","Chad","Chile","China","Colombia","Comoros","Congo","Congo Democratic Rep","Costa Rica","Croatia","Cuba","Cyprus","Czech Republic","Denmark","Djibouti","Dominica","Dominican Republic","East Timor","Ecuador","Egypt","El Salvador","Equatorial Guinea","Eritrea","Estonia","Ethiopia","Fiji","Finland","France","Gabon","Gambia","Georgia","Germany","Ghana","Greece","Grenada","Guatemala","Guinea","Guinea Bissau","Guyana","Haiti","Honduras","Hungary","Iceland","India","Indonesia","Iran","Iraq","Ireland","Israel","Italy","Ivory Coast","Jamaica","Japan","Jordan","Kazakhstan","Kenya","Kiribati","Kosovo","Kuwait","Kyrgyzstan","Laos","Latvia","Lebanon","Lesotho","Liberia","Libya","Liechtenstein","Lithuania","Luxembourg","Macedonia","Madagascar","Malawi","Malaysia","Maldives","Mali","Malta","Marshall Islands","Mauritania","Mauritius","Mexico","Micronesia","Moldova","Monaco","Mongolia","Montenegro","Morocco","Mozambique","Myanmar","Namibia","Nauru","Nepal","Netherlands","New Zealand","Nicaragua","Niger","Nigeria","North Korea","Norway","Oman","Pakistan","Palau","Panama","Papua New Guinea","Paraguay","Peru","Philippines","Poland","Portugal","Qatar","Romania","Russia","Rwanda","St Kitts and Nevis","St Lucia","Saint Vincent and the Grenadines","Samoa","San Marino","Sao Tome and Principe","Saudi Arabia","Senegal","Serbia","Seychelles","Sierra Leone","Singapore","Slovakia","Slovenia","Solomon Islands","Somalia","South","Africa","South Korea","South Sudan","Spain","Sri Lanka","Sudan","Suriname","Swaziland","Sweden","Switzerland","Syria","Taiwan","Tajikistan","Tanzania","Thailand","Togo","Tonga","Trinidad and Tobago","Tunisia","Turkey","Turkmenistan","Tuvalu","Uganda","Ukraine","United Arab Emirates","United Kingdom","United States","Uruguay","Uzbekistan","Vanuatu","Vatican City","Venezuela","Vietnam","Yemen","Zambia","Zimbabwe"]
 
 var vArrCountry=[];
 var vCountry="";
@@ -13,25 +13,38 @@ var vArrInput=[];
 var vArrGuess=[];
 var newWord = true;
 var vInputKeyCode;
+var vInputWord="";
+
+function toUpCase( strWord ) {
+    var strUC = strWord.charAt(0).toUpperCase();
+    strWord = strUC + strWord.substring(1, strWord.length);
+    for( var i=1; i<strWord.Length; i++ ) {
+        if( strWord[i]==' ') {
+            str
+        }
+    }
+
+
+}
 
 
 function checkWord() {
-    var vword =vArrInput.join();
-    console.log("97) here" + vword);
+    vInputWord =vArrInput.join();
+    console.log("97) here" + vInputWord);
     console.log("98): "+ vArrInput.join());
     console.log("99): "+vCountry.toLowerCase());
-    vword = vword.replace(/,/g, '');
-    console.log("100): "+vword);
+    vInputWord = vInputWord.replace(/,/g, ' ');
+    console.log("100): "+vInputWord);
     if( vArrInput.join().replace(/,/g,'')===vCountry.toLowerCase() ) {
         vWins++;
         vResult="You are Winner!" + arrCountries[vRandomNum];
-        vGuessesLeft=10;
+        // vGuessesLeft=10;
         newWord=true; 
         console.log("103)word matched "+ vWins)
     } else if( vGuessesLeft===0) {
         vLoss++;
         vResult="You Lost!" +"\n"+ "Word: "+vCountry;
-        vGuessesLeft=10;
+        // vGuessesLeft=10;
         newWord=true; 
         console.log("No match");
         console.log("104) No Match and no guessesleft");
@@ -56,8 +69,9 @@ function functionHangMan( event ) {
     console.log("38)input char keycode: "+ vInputKeyCode);
     console.log( "39)check match: " + String.fromCharCode(vInputKeyCode));
     console.log( "40)check match2: " + String.fromCharCode(vInputKeyCode).match(/[a-zA-Z]/));
-    if( vGuessesLeft === 10 && newWord === true ) {
+    if( newWord === true ) {
         newWord = false;
+        vGuessesLeft=10;
         vResult = "";
         vArrGuess.length=0;
         vArrInput.length=0;
@@ -75,7 +89,7 @@ function functionHangMan( event ) {
             else
                 vArrInput.push(" ");
         }
-        console.log("60): "+vArrInput.join());
+        console.log("60): "+vArrInput);
     }
     console.log("62)input char:"+vInput);
     if( String.fromCharCode(vInputKeyCode).match(/[a-zA-Z]/) !=null  && vArrGuess.indexOf(vInput) === -1) {
@@ -100,11 +114,16 @@ function functionHangMan( event ) {
         
     }
 
-      
-    spanInput.innerText = vArrInput.join().replace(/,/g,' ');
+    //vInputWord.replace(/" "/g, ' - ' );
+    console.log("150) vinputword: "+ vInputWord);
+    // alert(vInputWord);
+    spanInput.innerHTML = "<pre>"+ vArrInput.join().replace(/,/g,' ') +"</pre>";
+    // spanInput.innerText = vInputWord.slice(0,vInputWord.indexOf(" ")) 
+    //                         + "  " 
+    //                         + vInputWord.slice(vInputWord.indexOf(" ")+1, vInputWord.length-1 ) ;
     spanWins.innerText = vWins;
     spanLoses.innerText = vLoss;
-    spanResult.innerText = vResult; 
+    spanResult.innerText = vResult;
     spanGuessesLeft.innerText= vGuessesLeft;
     spanGuess.innerText = vArrGuess.join();
 
